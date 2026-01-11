5cd1ae19ca185f154a90256363261fda2f236db8
Max <maxslotov@icloud.com>
2026-01-11 03:53:16 +0200

    refactor: remove default destructor from BusT4Component

--- components/bus_t4/bus_t4_component.h
+++ components/bus_t4/bus_t4_component.h
@@ -11,7 +11,6 @@ namespace esphome::bus_t4 {
 class BusT4Component final : public Component, public uart::UARTDevice {
  public:
   BusT4Component() = default;
-  ~BusT4Component() override = default;
 
   void setup() override;
   void dump_config() override;
